85a322d25a3881bbb509ce056f5f07c5afc32029
luizddev21 <luiz.oliveira3386@outlook.com>
2026-07-05 09:54:30 -0300

    First Commit

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        msg: "API Funcionando"
+    });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log("Servidor rodando na porta", PORT);
+});
