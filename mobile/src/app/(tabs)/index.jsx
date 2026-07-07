032736e62314da584740f940068464023c9f8a9a
luizddev21 <luiz.oliveira3386@outlook.com>
2026-07-06 22:11:37 -0300

    Scroll Banner added to screen component

--- mobile/src/app/(tabs)/index.jsx
+++ mobile/src/app/(tabs)/index.jsx
@@ -1,11 +1,14 @@
-import { Text } from 'react-native';
-import Screen from '@/components/Screen';
+import { Text, View } from "react-native";
+import Screen from "@/components/Screen";
 
+import mainBanner from '@/assets/images/main_banner.png';
 
 export default function HomeScreen() {
+  
+
   return (
-    <Screen>
-      <Text>Home Screen</Text>
+    <Screen banner={mainBanner}>
+      
     </Screen>
   );
 }
--- mobile/src/components/Screen.jsx
+++ mobile/src/components/Screen.jsx
@@ -1,9 +1,76 @@
 import { View, StyleSheet } from "react-native";
 import { colors } from "@/theme/colors";
 import { useFonts } from '@expo-google-fonts/nunito';
+import Animated, {
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  interpolate,
+  Extrapolation,
+} from "react-native-reanimated";
 
-export default function Screen({ children }) {
-  return <View style={styles.container}>{children}</View>;
+const HEADER_HEIGHT = 250;
+
+export default function Screen({ children, banner = null }) {
+
+  const scrollY = useSharedValue(0);
+
+  const onScroll = useAnimatedScrollHandler((e) => {
+    scrollY.value = e.contentOffset.y;
+  });
+
+  const bannerStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateY: interpolate(
+            scrollY.value,
+            [0, HEADER_HEIGHT],
+            [0, -120],
+            Extrapolation.CLAMP,
+          ),
+        },
+      ],
+      opacity: interpolate(
+        scrollY.value,
+        [0, HEADER_HEIGHT * 0.8],
+        [1, 0],
+        Extrapolation.CLAMP,
+      ),
+    };
+  });
+
+  return (
+    banner ? 
+      <View style={styles.container}>
+      <Animated.Image
+        source={banner}
+        style={[
+          {
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            height: HEADER_HEIGHT,
+            width: '100%',
+            backgroundColor: "#6C63FF",
+            boxShadow: 'inset 0px 2px 8px rgba(0, 0, 0, 0.15)',
+            zIndex: 1,
+          },
+          bannerStyle,
+        ]}
+      />
+
+      <Animated.ScrollView onScroll={onScroll} scrollEventThrottle={16}>
+        <View style={{ height: HEADER_HEIGHT }} />
+        {children}
+      </Animated.ScrollView>
+    </View>
+    :
+    <View style={styles.container}>
+      {children}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
